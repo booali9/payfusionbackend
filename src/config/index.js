1899d88cab3fb57188a149a3258d6a03836f1132
Minhal Rizvi <rizvi4600779@cloud.neduet.edu.pk>
2025-03-11 23:36:08 +0500

    index.js code push

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpire: process.env.JWT_EXPIRE,
+    emailService: process.env.EMAIL_SERVICE,
+    emailUsername: process.env.EMAIL_USERNAME,
+    emailPassword: process.env.EMAIL_PASSWORD,
+    emailFrom: process.env.EMAIL_FROM
+  };
